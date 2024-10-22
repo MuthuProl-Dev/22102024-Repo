160c690670e50d7bbe71cf0bbd1450070b039ba5
MuthuProl-Dev <muthupandi@prolaborate.com>
2024-10-22 15:42:24 +0530

    query updated

--- Script.sql
+++ Script.sql
@@ -2,4 +2,5 @@
 -- Data for Employees
   Insert into Employees (ID,FirstName,LastName,Department) values ('1','Muthu','Pandi','IT')
   Insert into Employees (ID,FirstName,LastName,Department) values ('2','Arshad','Ahamed','IT')
+  Insert into Employees (ID,FirstName,LastName,Department) values ('3','Ravi','Kalyan','IT')
 
